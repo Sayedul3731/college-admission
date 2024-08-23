8151cd3070b4e55a976574d1c55a70c3907fdf91
MD SAYEDUL HAQUE <125432652+Sayedul3731@users.noreply.github.com>
2024-08-23 14:48:44 +0600

    Footer added

--- college-admission/src/Shared/Footer.jsx
+++ college-admission/src/Shared/Footer.jsx
@@ -1,11 +1,24 @@
 
-
-const Footer = () => {
+export default function Footer() {
     return (
-        <div>
-            <h1>This is Footer</h1>
-        </div>
+        <footer className="flex flex-col justify-around gap-5 bg-gray-300 py-8 dark:bg-gray-500 dark:text-white">
+            <h5 className="text-2xl font-bold text-center">College-Admission</h5>
+            <nav className="text-lg">
+                <ul className="flex h-full flex-wrap items-center justify-center gap-3">
+                    <li>
+                        <a className="cursor-pointer hover:underline">Home</a>
+                    </li>
+                    <li>
+                        <a className="cursor-pointer hover:underline">Contact</a>
+                    </li>
+                    <li>
+                        <a className="cursor-pointer hover:underline">About</a>
+                    </li>
+                </ul>
+            </nav>
+            <aside className="text-center text-sm">
+                <p>&copy; 2024 College-Admission. All Rights Reserved.</p>
+            </aside>
+        </footer>
     );
-};
-
-export default Footer;
+}
